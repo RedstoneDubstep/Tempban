bcc5f2c3f53e350b6bc9d2d48e6cd0c871b05b46
Redstone_Dubstep <perkhofer.lukas@gmail.com>
2021-12-01 18:56:27 +0100

    Updated to 1.18

--- src/main/java/redstonedubstep/mods/tempban/Tempban.java
+++ src/main/java/redstonedubstep/mods/tempban/Tempban.java
@@ -5,12 +5,12 @@ import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod("tempban")
 public class Tempban {
 	public Tempban() {
-		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
 	}
 
